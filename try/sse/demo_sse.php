6ed97233d33d8e6b89a5a2584d32aa8d453d1700
Dhananjay Nakrani <dhananjaynakrani@gmail.com>
2015-01-24 13:22:02 -0800

    Add Tries/Tests.

---
+++ try/sse/demo_sse.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+
+// $time = date('r');
+// echo "data: The server time is: {$time}\n\n";
+echo "data: Haha! \n\n";
+flush();
+?>
